d3ce1590222f7a5abd23f70c6c5e37d1fcdd2ae9
Rian Perassoli <rian.perassoli@gmail.com>
2021-06-23 20:39:25 -0300

    Passa a buscar valores da api stripe após cadastro do produto no ambiente de testes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,14 +27,35 @@ export default function Home() {
 
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceID={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  /**
+   * CRIAR ARQUIVO .env.local NA RAIZ DA APLICACAO COM OS VALORES ABAIXO
+   * 
+   * STRIPE_API_KEY=sk_test_51J5f15Fqd745Cz5XrkoShkjzrr9cOXvZk7irt1Tzy8gjwKOzBzQ6cuWtCByCcdock7VmtJYAsc55pyLkT9MwUVvT00jCvA3q63
+   */
+  const price = await stripe.prices.retrieve('price_1J5f4lFqd745Cz5XH0vc5HNj')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: { product }
+  }
 }
