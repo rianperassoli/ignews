f8f899cd996f822d82b4704db64b1d7dd9831229
Rian Perassoli <rian.perassoli@gmail.com>
2021-06-24 20:12:30 -0300

    Passa a salvar o usuario no faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as queryFauna } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -9,4 +13,24 @@ export default NextAuth({
       scope: 'read:user'
     })
   ],
+  jwt: {
+    signingKey: process.env.JWT_SIGNING_KEY
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      try {
+        const { email } = user
+
+        await fauna.query(
+          queryFauna.Create(queryFauna.Collection('users'), {
+            data: { email }
+          })
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
