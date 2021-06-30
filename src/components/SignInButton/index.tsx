512cb4c2b83359473655b4cdea82d55d96ea3cad
Rian Perassoli <rian.perassoli@gmail.com>
2021-06-30 20:59:51 -0300

    Criada o redirecionamento para a tela de pagamentos e conectada a conta com o Stripe.
    Passa a adicionar o codigo de usuario do stripe no fauna DB para evitar duplicações no Stripe
    
    Criada a ação no botão subscribe para redirecionar para o Login ou pagamento

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,8 +7,6 @@ import styles from './styles.module.scss'
 export function SignInButton() {
   const [session] = useSession()
 
-  console.log(session);
-
   return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
@@ -25,6 +23,5 @@ export function SignInButton() {
       Sign in with Github
     </button>
   )
-
 
 }
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/client'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,10 +8,30 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceID }: SubscribeButtonProps) {
+  const [session] = useSession()
+
+  async function handleSubscribe() {
+    if (!session) {
+      return signIn('github')
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
